49b73a2ba85bc6ddf1b97185690299574aa9a9ab
JBuhay34 <jbuhay3@gmail.com>
2018-02-22 19:51:00 -0800

    Fixed bugs
    Made block move with constant velocity

--- Assets/Scripts/BlockMovement.cs
+++ Assets/Scripts/BlockMovement.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class BlockMovement : MonoBehaviour {
 
-	private GameObject myprefab;
+    public int numBlocks;
+    public GameObject[] blocks;
+    public GameObject block;
 	// Use this for initialization
 	void Start () {
-		myprefab = Resources.Load ("blocks") as GameObject;
-		for (int i = 0; i < 10; i++) {
-			GameObject block1 = Instantiate (myprefab);
-			block1.transform.position = new Vector3 (0, (i+1)*5,0);
+
+        blocks = new GameObject[numBlocks];
+
+        for (int i = 0; i < blocks.Length; i++) {
+
+            GameObject go = Instantiate(block, new Vector3(-6, (i) * 5, 0), Quaternion.identity) as GameObject;
+
+            blocks[i] = go;
 		}
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Rigidbody rb = blocks[0].GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(5, 0, 0);
+
+
 	}
 }
